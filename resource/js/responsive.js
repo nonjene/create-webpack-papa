57053d5cb84d007e3f0a54b870d05ecd0c699b14
nonjene <reanfn89@gmail.com>
2017-07-22 04:21:14 +0800

    format code

--- resource/js/responsive.js
+++ resource/js/responsive.js
@@ -2,23 +2,23 @@
  * Created by Nonjene on 2017/2/13.
  */
 // 设置页面rem大小
-(function (doc, win) {
-    if (win.limin.page.isPC) return;
+(function(doc, win) {
+    if (win.okPapa.page.isPC) return;
 
     var maxWidth = 750;
     var baseWidth = 750;
     var baseFontSize = 100;
-    var docEle      = doc.documentElement,
-        dpr         = Math.min(win.devicePixelRatio, 3),
-        scale       = 1 / dpr,
-        resizeEvent = 'orientationchange' in win ? 'orientationchange' : 'resize';
+    var docEle = doc.documentElement,
+        dpr = Math.min(win.devicePixelRatio, 3),
+        scale = 1 / dpr,
+        resizeEvent = "orientationchange" in win ? "orientationchange" : "resize";
 
     docEle.dataset.dpr = dpr;
     /*var metaEle = doc.createElement('meta');
     metaEle.name = 'viewport';
     metaEle.content = 'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale;
     docEle.firstElementChild.appendChild(metaEle);*/
-    var recalCulate = function () {
+    var recalCulate = function() {
         var width = docEle.clientWidth;
         if (width > maxWidth) {
             width = maxWidth;
@@ -26,7 +26,7 @@
         //if (width / dpr > maxWidth) {
         //    width = maxWidth * dpr;
         //}
-        docEle.style.fontSize = baseFontSize * width / baseWidth + 'px';
+        docEle.style.fontSize = baseFontSize * width / baseWidth + "px";
     };
     recalCulate();
     if (!doc.addEventListener) return;
